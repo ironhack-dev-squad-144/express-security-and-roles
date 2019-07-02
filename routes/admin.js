95231cda037126b3b9141ac272dd69de336a26ce
Maxence Bouret <maxence.bouret@gmail.com>
2019-07-02 17:50:16 +0100

    Done

--- routes/admin.js
+++ routes/admin.js
@@ -5,9 +5,27 @@ const { checkAdmin } = require("../middlewares");
 
 // Route GET /admin/pending-tshirts only for admins
 router.get("/pending-tshirts", checkAdmin, (req, res, next) => {
-  Tshirt.find({ isValidated: false }).then(tshirts => {
-    res.render("admin/pending-tshirts", { tshirts });
-  });
+  Tshirt.find({ isValidated: false })
+    .populate("_owner")
+    .then(tshirts => {
+      res.render("admin/pending-tshirts", { tshirts });
+    });
+});
+
+router.get("/validate-tshirt/:tshirtId", checkAdmin, (req, res, next) => {
+  Tshirt.findByIdAndUpdate(req.params.tshirtId, { isValidated: true })
+    .then(tshirt => {
+      res.redirect("/admin/pending-tshirts");
+    })
+    .catch(err => next(err));
+});
+
+router.get("/remove-tshirt/:tshirtId", checkAdmin, (req, res, next) => {
+  Tshirt.findByIdAndRemove(req.params.tshirtId)
+    .then(tshirt => {
+      res.redirect("/admin/pending-tshirts");
+    })
+    .catch(next);
 });
 
 module.exports = router;
